8d6239ce38886d21c37cdaafe571a877032bafaf
Yu Li <liyutommy@outlook.com>
2019-12-25 23:06:18 -0700

    adjust mobile font size

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: "Software Developer Blog",
     author: {
         name: "Yu Li",
-        bio: "I am a Computer Science undergraduate student at the University of Colorado at Boulder",
+        bio: "I am a Computer Science undergraduate student at University of Colorado Boulder",
         contacts: {
             linkedin: "https://www.linkedin.com/in/yu-li-877806140/",
             github: "https://github.com/liyutommy",
